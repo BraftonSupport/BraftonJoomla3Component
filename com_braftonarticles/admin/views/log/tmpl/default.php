a74f2c812fdf22d6737909fbfea6421e8a6801f9
dking3876 <dking3876@msn.com>
2015-09-17 13:34:25 -0400

    modify log view

--- com_braftonarticles/admin/views/log/tmpl/default.php
+++ com_braftonarticles/admin/views/log/tmpl/default.php
@@ -5,5 +5,7 @@ JHTML::_('behavior.tooltip');
 ?>
 <form action="index.php?option=com_braftonarticles&view=log" method="post" name="adminForm" id="adminForm">
     <input type="hidden" name="task" value="" />
-<pre><?php if ($this->logContents) echo $this->logContents; ?></pre>
+    <?php if ($this->logContents){ ?>
+    <pre><?php echo $this->logContents; ?></pre>
+    <?php } ?>
 </form>
