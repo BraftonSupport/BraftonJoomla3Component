abe7d7128aaa98f1985ede375af928cdff400cc1
dking3876 <dking3876@msn.com>
2015-09-17 12:45:05 -0400

    added 3.0.0.sql file for backwards compatibility

---
+++ com_braftonarticles/admin/sql/updates/mysql/3.0.0.sql
@@ -0,0 +1,1 @@
+INSERT IGNORE INTO `#__brafton_options` (`option`) VALUES ('secret-key'), ('public-key'), ('feed-number'), ('import-assets'), ('stop-importer'), ('pause-text'), ('pause-link'), ('pause-asset-id'), ('end-background'), ('end-title'), ('end-subtitle'), ('end-text'), ('end-link'), ('end-asset-id'), ('debug');
